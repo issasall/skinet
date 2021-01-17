0a7cc397112c8d8d347b977fd202468ec3b92115
issa sall <issasall@gmail.com>
2021-01-16 23:20:20 -0700

    initial commit

---
+++ API/Controllers/SQLite.sql
@@ -0,0 +1,6 @@
+-- SQLite
+INSERT INTO Products (Id, Name)
+VALUES (1, "Product One"),
+ (2, "Product Two"),
+ (3, "Product Three"),
+(4, "Product Four");
---
+++ Core/Entities/Products.cs
@@ -0,0 +1,8 @@
+namespace Core.Entities
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
